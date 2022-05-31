41a71a245c476638494b43f11cfe96fac3d61f36
maissa-gallah <maissaglh@gmail.com>
2022-05-31 17:44:46 +0200

    catch errors

--- merge.py
+++ merge.py
@@ -12,38 +12,39 @@ file_list = glob.glob(json_pattern)
 coordinates2018=[]
 coordinates2020=[]
 coordinates2022=[]
-
-for file in file_list:
+i =0
+for k in range (len (file_list) ):
+    file=file_list[k]
+    i=i+1
+    print(file)
     with open(file) as f:
         layer = json.load(f)
         for x in layer["features"]:
-            print(x["properties"]["date"])
             d=datetime.fromisoformat(x["properties"]["date"])
-            if (d.year==2018):
-                coordinates2018.append(x["geometry"]["coordinates"])
-            if (d.year==2020):
-                coordinates2020.append(x["geometry"]["coordinates"])
-            if (d.year==2022):
-                coordinates2022.append(x["geometry"]["coordinates"])
-f.close()
-fusion = {
-    'date 2018' : coordinates2018,
-    'date 2020' : coordinates2020,
-    'date 2022' : coordinates2022
-}
+            listCoordinate=x["geometry"]["coordinates"]
+            for k in range (len(listCoordinate)):
+                if (d.year==2018):
+                    coordinates2018.append(x["geometry"]["coordinates"][k])
+                elif (d.year==2020):
+                    coordinates2020.append(x["geometry"]["coordinates"][k])
+                elif (d.year==2022):
+                    coordinates2022.append(x["geometry"]["coordinates"][k])
+                else:
+                    print("doesnt aapend any think")
+        f.close()
 
-coordinates2018=coordinates2018[0]
+coordinates2018=coordinates2018
 mulipoint2018=MultiPoint(coordinates2018)
+
+coordinates2020=coordinates2020
+mulipoint2020=MultiPoint(coordinates2020)
+
+coordinates2022=coordinates2022
+mulipoint2022=MultiPoint(coordinates2022)
 
 features = []
 features.append(Feature(geometry=mulipoint2018, properties={"date": "2018"},))
-
-coordinates2020=coordinates2020[0]
-mulipoint2020=MultiPoint(coordinates2020)
 features.append(Feature(geometry=mulipoint2020, properties={"date": "2020"}))
-
-coordinates2022=coordinates2022[0]
-mulipoint2022=MultiPoint(coordinates2022)
 features.append(Feature(geometry=mulipoint2022, properties={"date": "2022"}))
 
 # add more features...
@@ -53,3 +54,4 @@ feature_collection = FeatureCollection(features)
 
 with open('tunisia.geojson', 'w') as f:
    dump(feature_collection, f)
+print(i)
